1ccd3cfb9668e221f6f7529979df915f359fd21b
joewzy <josiahkotey13@gmail.com>
2025-01-29 15:51:48 +0000

    Customer/User get rooms impl.

--- RoomBooking/src/main/java/com/hotel/RoomBooking/service/user/room/UserRoomService.java
+++ RoomBooking/src/main/java/com/hotel/RoomBooking/service/user/room/UserRoomService.java
@@ -1,4 +1,8 @@
 package com.hotel.RoomBooking.service.user.room;
 
+import com.hotel.RoomBooking.dto.RoomResponseDto;
+import com.hotel.RoomBooking.exceptions.RoomException;
+
 public interface UserRoomService {
+    RoomResponseDto getAvailableRooms(int pageNumber) throws RoomException;
 }
